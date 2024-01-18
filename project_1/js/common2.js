b20b97518866b3acdfa0a67a2b8f52e43b8ef18e
me5290 <78029136+me5290@users.noreply.github.com>
2024-01-18 09:32:24 +0900

    Delete project_1 directory

--- project_1/js/common2.js
+++
@@ -1,9 +0,0 @@
-// 헤더 푸터 불러오기
-// $( document ).ready( function(){
-//     $('#headers').load('../common/header.html');
-//     $('#footers').load('../common/footer.html');
-// });
-
-document.addEventListener('DOMContentLoaded',function(){
-    
-});
